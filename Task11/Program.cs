d8aada78f9d4e30a6febe386ffe596d05f4ab927
odrinskii <ananjev.arseniy@gmail.com>
2022-10-19 00:28:20 +0300

    Added Tasks 11 12

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 11. 
+// Напишите программу, которая 
+// 1. выводит случайное трехзначное число и 
+// 2. удаляет вторую цифру этого числа. 
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазона 100 - 999 -> {num}");
+
+int TwoDigits(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+int newDigit = TwoDigits(num);
+Console.WriteLine(newDigit);
---
+++ Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 12 Напишите программу, которая будет
+// 1. Принимать на вход два числа и
+// 2. Выводить, является ли первое число кратно второму.
+// 3. Если число 1 не кратно числу 2, то программа выводит остаток от деления
+
+
+int Div(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int rest = Div(a, b);
+
+Console.WriteLine(rest == 0? "Кратно" : $"Не кратно, остаток - {rest}"); 
+
+
+
+
