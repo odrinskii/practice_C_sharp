15a607280e742c685d1987d46dedbbf058c77877
odrinskii <ananjev.arseniy@gmail.com>
2022-10-19 20:29:53 +0300

    added task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая
+// 1. Принимает на вход число и 
+// 2. Проверяет кратно ли оно одновременно 7 и 23
+
+int DivOption(int num)
+{
+    int result;
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        result = 1;
+    }
+    else result = -1;
+    return result;
+}
+
+
+Console.Write("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int decision = DivOption(number);
+Console.WriteLine(decision == 1 ? "Да" : "Нет");
