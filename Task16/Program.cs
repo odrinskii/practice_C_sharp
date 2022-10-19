4c75942eb49d5779286c17f0d27249c1a55cd324
odrinskii <ananjev.arseniy@gmail.com>
2022-10-20 00:10:32 +0300

    Added Task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая 
+// 1. Принимает на вход два числа
+// 2. Проверяет является ли одно число квадратом другого
+
+
+bool MultCheck(int a, int b)
+{
+    return a * a == b || b * b == a;
+}
+
+Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square = MultCheck(num1, num2);
+Console.WriteLine(square ? "Да" : "Нет");
