0e2ad7eda061d56eada22c017f4e66fc73911b84
odrinskii <ananjev.arseniy@gmail.com>
2022-10-19 20:37:21 +0300

    edited task 14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -2,19 +2,23 @@
 // 1. Принимает на вход число и 
 // 2. Проверяет кратно ли оно одновременно 7 и 23
 
-int DivOption(int num)
+// int DivOption(int num)
+// {
+//     int result;
+//     if (num % 7 == 0 && num % 23 == 0)
+//     {
+//         result = 1;
+//     }
+//     else result = -1;
+//     return result;
+// }
+bool DivOption(int num)
 {
-    int result;
-    if (num % 7 == 0 && num % 23 == 0)
-    {
-        result = 1;
-    }
-    else result = -1;
-    return result;
+    return num % 7 == 0 && num % 23 == 0;
 }
 
 
 Console.Write("Введите целое число:");
 int number = Convert.ToInt32(Console.ReadLine());
-int decision = DivOption(number);
-Console.WriteLine(decision == 1 ? "Да" : "Нет");
+bool multpl = DivOption(number);
+Console.WriteLine(multpl ? "Да" : "Нет");
