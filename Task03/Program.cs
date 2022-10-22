c2b99a19bc612a1ab9c9c9bcf8260d81dc26c301
odrinskii <ananjev.arseniy@gmail.com>
2022-10-23 00:12:25 +0300

    Added Task10

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -8,13 +8,13 @@ int day = Convert.ToInt32(Console.ReadLine());
 
 if (day > 0 && day < 8)
 {
-    if (day == 1) ConsoleWrite("Понедельник");
-    else if (day == 2) ConsoleWrite("Вторник"); 
+    if (day == 1) Console.Write("Понедельник");
+    else if (day == 2) Console.Write("Вторник"); 
     // используем else if для того, чтобы программа не проверяла дальше в случае выполнения условия
-    else if (day == 3) ConsoleWrite("Среда");
-    else if (day == 4) ConsoleWrite("Четверг");
-    else if (day == 5) ConsoleWrite("Пятница");
-    else if (day == 6) ConsoleWrite("Суббота");
-    else ConsoleWrite("Воскресенье");
+    else if (day == 3) Console.Write("Среда");
+    else if (day == 4) Console.Write("Четверг");
+    else if (day == 5) Console.Write("Пятница");
+    else if (day == 6) Console.Write("Суббота");
+    else Console.Write("Воскресенье");
 }
 else Console.Write("Такого дня недели не существует");
