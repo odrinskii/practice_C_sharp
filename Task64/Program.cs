72bfaabdeb052f844cd8bb88a6a540a8ddc52ce9
odrinskii <ananjev.arseniy@gmail.com>
2022-11-29 07:09:08 +0300

    Added homework Task64, 66

---
+++ Task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNumbers(int num)
+{
+    if(num == 1) 
+    {
+        Console.Write($"{num}'"); 
+        return;  
+    } 
+    Console.Write($"{num},");  
+    NaturalNumbers(num - 1); // хвостовая рекурсия
+}
+
+
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+Console.Write($"N = {number} -> '");
+NaturalNumbers(number);
+Console.WriteLine();
+}
+else Console.WriteLine("Ошибка! Введите натуральное число.");
---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int SumNumbersMtoN(int m, int n)
+{
+    if (m == n) return m;
+    if (m < n) return m + SumNumbersMtoN(m + 1, n);
+    else return n + SumNumbersMtoN(m - 1, n);
+}
+
+Console.WriteLine("Введите два числа друг за другом: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a >= 0 && b >= 0)
+{
+    int result = SumNumbersMtoN(a, b);
+    Console.WriteLine($"Сумма элементов от {a} до {b} равна {result}");
+}
+else Console.WriteLine("Ошибка! Введите натуральные числа.");
